dfe223c8acdd939935c8ee6b7d30c882c91cca8e
KhoaNguyen <khoa24092000@gmail.com>
2021-06-30 15:00:17 +0700

    trang 35

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using NguyenDucCongKhoa_BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace NguyenDucCongKhoa_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Course
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
