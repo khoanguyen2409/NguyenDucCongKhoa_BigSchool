682c9de649addd16d9faf210c473cf2d5409abe7
KhoaNguyen <khoa24092000@gmail.com>
2021-07-03 17:29:12 +0700

    3/7 trang 44

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using NguyenDucCongKhoa_BigSchool.DTOs;
+using NguyenDucCongKhoa_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NguyenDucCongKhoa_BigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendace already exists!");
+            var attendace = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendace);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
