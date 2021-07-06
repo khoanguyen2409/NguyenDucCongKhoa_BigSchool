7e34e3ead1a77a681b943a8c4cb116376afc62ff
KhoaNguyen <khoa24092000@gmail.com>
2021-07-06 19:58:20 +0700

    xong /62

---
+++ Controllers/Api/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using NguyenDucCongKhoa_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NguyenDucCongKhoa_BigSchool.Controllers.Api
+{
+    public class CoursesController : ApiController
+    {
+        public ApplicationDbContext _dbContext { get; set; }
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Course.Single(c => c.Id == id && c.LecturerId == userId);
+            if (course.IsCanceled)
+                return NotFound();
+            course.IsCanceled = true;
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
